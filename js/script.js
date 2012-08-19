bf481444e9132d6828de4c32ff40da16d8160cf5
Dmitry Kanatnikov <steve111@yandex.ru>
2012-08-20 00:17:57 +0400

    Добавлен аккордеон по разделам, подсветка кода(highlight.js) и кнопка свернуть/развернуть код.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
 jQuery(document).ready(function($) {
+	hljs.initHighlighting();
 
+	/* Если отключен js, все будет видно */
+	$('.submenu').hide();
+	$('.show-code').show();
+	$('pre').hide();
+
+	$('.desc').on('click', '.show-code', function(e) {
+	    var $this = $(this);
+	    $this
+	    	.next("pre")
+	    	.slideToggle();
+	    $("span", $this).toggleClass("hide");
+
+		e.stopPropagation();
+		e.preventDefault();
+	});
+
+	$('#sidebar').on('click', '.main-nav > li > a', function(e) {
+		$(this)
+			.closest('li')
+			.find('.submenu')
+			.slideToggle();
+
+		e.stopPropagation();
+		e.preventDefault();
+	});
 });
